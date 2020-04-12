78ffb23e1021bfc0c95164cdd492a4c38db98035
chitra <chitraflorid@users.noreply.github.com>
2020-04-13 00:11:02 +0530

    Update recursive-solutions.js
    Added function 'flattenObj' to flatten the given deeply nested object.

--- recursive-solutions.js
+++ recursive-solutions.js
@@ -135,3 +135,76 @@ function findNumber(target, arr) {
 
 findNumber(90, inputArr);
 
+// flatten the object
+// Input
+
+// Output should be
+// // {
+//         'user_name' : 'chitra',
+//         'user_address_home_doorno': '19',
+//         'user_address_home_street': '5th main',
+//         'user_address_home_area_name': 'JR Greenpark',
+//         'user_address_home_area_landmark': 'Opp to YKR Kalyan mandir',
+//         'user_address_home_post': 'Marsur',
+//         'user_address_home_city': 'Bangalore',
+//         'user_address_home_pin': '562106',
+//         'user_address_office_org': 'Vmware India',
+//         'user_address_offie_area_name': 'Salarpuria softzone, Bellandur',
+//         'user_address_office_area_landmark': 'near Iggalur lake',
+//         'user_address_office_post': 'Belandur',
+//         'user_address_office_city': 'Bangalore',
+//         'user_address_office_pin': '34343232',
+// // }
+const user = {
+    name: 'chitra',
+    address: {
+        home: {
+            doorno: '19',
+            street: '5th main',
+            area: {
+                name: 'JR GreenPark',
+                landmark: 'Opp to YKR Kalyan mandir',
+            },
+            post: 'Marsur',
+            city: 'Bangalore',
+            pin: '562106',
+        },
+        office: {
+            org: 'VMware India',
+            area: {
+                name: 'Salarpuria softzone, Bellandur',
+                landmark: 'near iggalur lake',
+            },
+            post: 'Bellandur',
+            city: 'Bangalore',
+            pin: '343432432',
+        },
+    },
+};
+
+const flattenedObj = {};
+
+function flattenObj(node, prefix = 'user') {
+    if (!node) {
+        return;
+    }
+
+    if (typeof node === 'object') {
+        Object.keys(node).forEach(key => {
+            const value = node[key];
+
+            key = `${prefix}_${key}`;
+
+            if (value && typeof value === 'object') {
+                return flattenObj(value, key);
+            } else {
+                flattenedObj[key] = value;
+            }
+        });
+    }
+
+    return flattenedObj;
+}
+
+flattenObj(user);
+

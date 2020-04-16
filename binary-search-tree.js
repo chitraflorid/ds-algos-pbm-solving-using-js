6c1f347023e4dff888373a0554b6a8893075d403
chitra <chitraflorid@users.noreply.github.com>
2020-04-16 20:27:47 +0530

    Update binary-search-tree.js
    Added the following methods 
    delete  -  to delete the given value from BST.
    findNode - to find out the node in the BST based on the given value.
    findParent - to find out the parentNode of the given node.
    createInorderList - to create InOrder traversal list for the current BST

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -7,13 +7,48 @@ class BSTNode {
         this.left = null;
         this.right = null;
     }
+
+    /**
+     * Checks whether the current node is leaf node or not.
+     * @return {boolean}
+     */
+    isLeafNode() {
+        if (!this.left && !this.right) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Checks whether the node has only one child.
+     * @return {boolean}
+     */
+    hasOnlyOneChild() {
+        if (this.left && !this.right || this.right && !this.left) return true;
+
+        return false;
+    }
+
+    hasLeftNode() {
+        return !!this.left;
+    }
+
+    hasRightNode() {
+        return !!this.right;
+    }
 }
 
 class BST {
     constructor() {
         this.root = null;
+        this.inOrderList = [];
     }
 
+    /**
+     * Inserts the given value to the BST.
+     * @param data
+     */
     insert(data) {
         const { root } = this;
 
@@ -24,6 +59,72 @@ class BST {
         }
     }
 
+    /**
+     * Returns the node when the given data is present in BST.
+     * @param data
+     * @param {BSTNode} root - current root node
+     * @return {BSTNode}
+     */
+    findNode(data, root) {
+        if (root.data === data) {
+            return root;
+        }
+
+        if (data < root.data) {
+            root = root.left;
+        } else {
+            root = root.right;
+        }
+
+        return this.findNode(data, root);
+    }
+
+    /**
+     * returns the parent node for the given value if its found in the BST.
+     * @param data
+     * @param {BSTNode} root
+     * @return {BSTNode}
+     */
+    findParent(data, root) {
+        if (!root) {
+            return;
+        }
+
+        const { left = {}, right = {} } = root;
+
+        if (left && left.data === data || right && right.data === data) {
+            return root;
+        }
+
+        if (data < root.data) {
+            root = left;
+        } else {
+            root = right;
+        }
+
+        return this.findParent(data, root);
+    }
+
+    /**
+     * Creates InOrder list of the BST nodes.
+     */
+    createInorderList(node) {
+        if (!node) {
+            return;
+        }
+
+        const { left, right } = node;
+
+        this.createInorderList(left);
+        this.inOrderList.push(node.data);
+        this.createInorderList(right);
+    }
+
+    /**
+     * Updates the BST with the given node at its right position.
+     * @param {BSTNode} root
+     * @param {BSTNode} newNode
+     */
     updateBST(root, newNode) {
         const { data: rootData } = root;
         const { data: nodeVal } = newNode;
@@ -52,11 +153,51 @@ class BST {
 
         this.updateBST(root, newNode);
     }
+
+    /**
+     * deletes the given value from the BST.
+     * @param {number} data
+     */
+    delete(data) {
+        const currentNode = this.findNode(data, this.root);
+        const parent = this.findParent(data, this.root);
+        const isLeftChild = parent.left && parent.left.data === currentNode.data;
+
+        if (currentNode.isLeafNode()) {
+            if (isLeftChild) {
+                parent.left = null;
+            } else {
+                parent.right = null;
+            }
+        }
+
+        if (currentNode.hasOnlyOneChild()) {
+            if (currentNode.hasLeftNode()) {
+                parent.left = currentNode.left;
+            } else {
+                parent.right = currentNode.right;
+            }
+        } else {
+            this.createInorderList(this.root);
+
+            const predecessor = this.inOrderList[this.inOrderList.indexOf(data) - 1];
+            const predecessorParent = this.findParent(predecessor, this.root);
+
+            if (predecessorParent.left && predecessorParent.left.data === predecessor) {
+                predecessorParent.left = null;
+            } else {
+                predecessorParent.right = null;
+            }
+
+            currentNode.data = predecessor;
+        }
+
+        return true;
+    }
 }
 
 const bsTree = new BST();
 
 [11, 6, 8, 19, 4, 10, 5, 17, 43, 49, 31].forEach(item => bsTree.insert(item));
 
-console.log( "BST Tree::");
-console.log(bsTree);
+bsTree.delete(6);

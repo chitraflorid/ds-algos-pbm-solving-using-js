9f79cd45d7df0c4beac20c0892593b2fe53d6aa2
chitra <chitraflorid@users.noreply.github.com>
2020-04-22 13:12:21 +0530

    Create selection-sort.js
    Added selection sort implementation using recursive

---
+++ selection-sort.js
@@ -0,0 +1,28 @@
+// selection sort
+const arrInput = [26, 54, 93, 17, 77, 31, 44, 55, 20]; //[20, 12, 10, 15, 2];
+const len = arrInput.length;
+
+function selectionSort(startIndex = 0) {
+    if (startIndex >= len) return;
+
+    let minIndex = startIndex;
+    let minEle = arrInput[minIndex];
+
+    for (let i = startIndex + 1; i < len; i++) {
+        if (arrInput[i] < minEle) {
+            minEle = arrInput[i];
+            minIndex = i;
+        }
+    }
+
+    if (minIndex > startIndex) {
+        [arrInput[minIndex], arrInput[startIndex]] = [arrInput[startIndex], arrInput[minIndex]];
+    }
+
+    selectionSort(startIndex + 1);
+}
+
+selectionSort();
+
+console.log("sorted Arr:");
+console.log(arrInput);

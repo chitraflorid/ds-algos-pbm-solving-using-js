5459cf035d9e9f56c74f794bd85257ff95c09c80
chitra <chitraflorid@users.noreply.github.com>
2020-05-14 21:44:22 +0530

    Added a function with time complexity as O(n*k) to find the max sum of subarray with size k

--- pbm-solving-with-array.js
+++ pbm-solving-with-array.js
@@ -18,3 +18,27 @@ function find2ndLargestEle(inputArr) {
 }
 
 find2ndLargestEle(arr);
+
+// Find the Maximum sum of a contiguous SubArray of Size k - Time Complexity : O(n*k).
+function findMaxSumOfSubArray(arr, k) {
+    let sum = 0;
+    let currentSum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        const winSize = i + k;
+        let j = i + 1;
+
+        currentSum = arr[i];
+
+        while (j < winSize) {
+            currentSum += arr[j];
+            j++;
+        }
+
+        if (currentSum > sum) {
+            sum = currentSum;
+        }
+    }
+
+    return sum;
+}

2b97246b7f61eaf54fe852574978de0509b7829b
chitra <chitraflorid@users.noreply.github.com>
2020-05-19 19:42:01 +0530

    Added a function "oddEvenList"
    oddEvenList is used to group all odd nodes together followed by the even nodes.

--- pbm-solving-with-linkedlist.js
+++ pbm-solving-with-linkedlist.js
@@ -317,3 +317,46 @@ function getNodesCount(head) {
     return nodesArr;
 }
 
+    let evenList = null;
+    // Odd Even Linked List. 
+   // Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+//You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+// Ex: Input: 1->2->3->4->5->NULL
+// Output: 1->3->5->2->4->NULL
+    function oddEvenList(head) {
+        let current = head;
+        let prev = current;
+        if (!head || !head.next) return head;
+        
+        while (current && current.next) {
+            prev = current;
+            const even = current.next;
+            const odd = current.next.next;
+            even.next = null;
+            appendEvenList(even);
+            current.next = odd;
+            current = current.next;
+        }
+        
+        while (prev.next) {
+            prev = prev.next;
+        }
+        
+        prev.next = evenList;
+        evenList = null;
+        return head;
+};
+
+function appendEvenList(node) {        
+    if (!evenList) {
+        evenList = node;
+    } else {
+        let curr = evenList;
+        while (curr.next) {
+            curr = curr.next;
+        }
+        
+        curr.next = node;
+    }
+}

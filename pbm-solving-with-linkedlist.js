f230f2f76234eaa35f6c7fe03f1662e2a53da4b8
chitra <chitraflorid@users.noreply.github.com>
2020-05-20 20:02:25 +0530

    Added splitListToParts to split the given linked list.
    splitListToParts used to split the linked list into k consecutive linked list "parts"

--- pbm-solving-with-linkedlist.js
+++ pbm-solving-with-linkedlist.js
@@ -360,3 +360,53 @@ function appendEvenList(node) {
         curr.next = node;
     }
 }
+/**
+Given a (singly) linked list with head node root, write a function to split the linked list into k consecutive linked list "parts".
+
+The length of each part should be as equal as possible: no two parts should have a size differing by more than 1. This may lead to some parts being null.
+
+The parts should be in order of occurrence in the input list, and parts occurring earlier should always have a size greater than or equal parts occurring later.
+
+Return a List of ListNode's representing the linked list parts that are formed.
+
+Examples 1->2->3->4, k = 5 // 5 equal parts [ [1], [2], [3], [4], null ]
+**/
+function splitListToParts(root, k) {
+    const count = getNodesCount(root);
+    const listArr = [];
+    const listCount = Math.floor(count / k);
+    const rem = count % k;
+    let current = root;
+    
+    for (let i = 0; i < k; ++i) {
+        let tmp = new ListNode(null);
+        let tmp2 = tmp;
+        if (current) {
+            for (let j = 0; j < listCount + ( i < rem ? 1 : 0); ++j) { 
+                if (current) {
+                    tmp2 = tmp2.next = new ListNode(current.val);
+                    current = current.next;    
+                }
+            }
+            
+            listArr.push(tmp.next);
+        } else {
+            listArr.push(null)
+        }
+        
+    }
+    
+    return listArr;
+}
+
+function getNodesCount(root) {
+    let count = 0;
+    let current = root;
+    
+    while (current) {
+        count++;
+        current = current.next;
+    }
+    
+    return count;
+}
